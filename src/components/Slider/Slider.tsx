91fa5f5216a24b7b98f983f2b420456f1e024005
Ksena <ksena@MacBook-Pro-Ks.local>
2022-12-07 08:37:54 +0200

    continue creating the slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -2,16 +2,27 @@ import React from 'react'
 
 import swipeLeft from 'img/svg/swipe-left.svg'
 import swipeRight from 'img/svg/swipe-right.svg'
-import sliderPic from 'img/jpg/living-room-pic-1.jpg'
+import sliderPic1 from 'img/jpg/living-room-pic-1.jpg'
+import sliderPic2 from 'img/jpg/bad-room-pic-1.jpg'
+import sliderPic3 from 'img/jpg/dining-room-pic-1.jpg'
 
 import styles from './Slider.module.scss'
 
 export const Slider = () => {
+
+    const sliderPics = [sliderPic1,sliderPic2,sliderPic3]
+
     return (
         <div className={styles.wrapper}>
-            <img src={sliderPic} alt="slider" />
-            <img src={swipeLeft} alt="slider" className={styles.swipe_left}/>
-            <img src={swipeRight} alt="slider" className={styles.swipe_right}/>
+            <div className={styles.sliderPics}>
+                {sliderPics.map(pic =>
+                    <img src={pic} alt="pic" />
+                )}
+            </div>
+            <div className={styles.swipeWrapper}>
+                <img src={swipeLeft} alt="swipeLeft" className={styles.swipeLeft}/>
+                <img src={swipeRight} alt="swipeRight" className={styles.swipeRight}/>
+            </div>
         </div>
     )
 }
